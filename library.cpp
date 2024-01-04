f56cd13374e30d4644c50340be8ef7a3d04b3c9c
Deyvid Vasilev <deyvidapps@gmail.com>
2024-01-04 14:17:48 +0000

    Implementing the Person class

--- library.cpp
+++ library.cpp
@@ -7,6 +7,44 @@
 
 #include <iostream>
 
+class Person {
+   private:
+    std::string name;
+    std::string address;
+    std::string email;
+
+   public:
+    Person() = default;
+    Person(const std::string& name, const std::string& address, const std::string& email)
+        : name(name), address(address), email(email) {}
+
+    std::string getName() const {
+        return name;
+    }
+    void setName(const std::string& newName) {
+        name = newName;
+    }
+
+    std::string getAddress() const {
+        return address;
+    }
+    void setAddress(const std::string& newAddress) {
+        address = newAddress;
+    }
+
+    std::string getEmail() const {
+        return email;
+    }
+    void setEmail(const std::string& newEmail) {
+        email = newEmail;
+    }
+};
+
 int main() {
-    std::cout << "Test";
+    // Testing the Person class
+
+    Person person("Dan Man", "376 Fake St", "random@gmail.com");
+    std::cout << "Person details: " << person.getName() << ", " << person.getAddress() << ", " << person.getEmail() << std::endl;
+
+    return 0;
 }
